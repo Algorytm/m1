37ab6707c6a6ad831e7d6b7157f9f73f2e3fefec
Przemysław Zapaśnik <przemozeus@outlook.com>
2017-07-02 13:57:03 +0200

    Bootstrap

---
+++ TheWorld/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public interface IMailService
+    {
+        void SendMail(string to, string ftom, string subject, string body);
+    }
+}
